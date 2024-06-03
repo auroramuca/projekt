ba1ea6a0f18acc215596a99a2f3c5a386eea91fb
Amanda Isufi <amaisu18@htl-shkoder.com>
2024-06-03 12:20:02 +0200

    neu funktion dontknow

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
+    public int dontknow(){
+        //nddsd
+        return 0;
+    }
     public static void main(String[] args) {
 
     }
