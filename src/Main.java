3b3aabe7c7637250b080a463671e4fed160afb59
Aurora Muca <aurmuc18@htl-shkoder.com>
2024-06-03 12:48:01 +0200

    neue Funktion

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
 public class Main {
-    public int triangle(int b, int h){
+    public int trianglesurface(int b, int h){
         return (b*h)/2;
+    }
+    public int triangleperimeter(int a, int b){
+        return 2*a+b;
+    }
+    public int trapezoidsurface(int a, int b, int h){
+        return ((a+b)/2)*h;
     }
     public static void main(String[] args) {
 
